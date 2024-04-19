dbca4b81e87278e06117ec6a764155d842f3a348
Tarun Reddy <132771817+Tarun6062@users.noreply.github.com>
2024-04-19 21:23:38 +0530

    Update t20_wc_match_results.js

--- t20_wc_match_results.js
+++ t20_wc_match_results.js
@@ -5,7 +5,7 @@
 navigate('https://stats.espncricinfo.com/ci/engine/records/team/match_results.html?id=14450;type=tournament');
 collect(parse());
 
-
+ 
 
 
 //------- 1.b Parser Code ------------//
